fb4d895d24d71b9af7e68f1fd554975ba576a5e6
aleexvdev <valverdealex25@gmail.com>
2024-08-27 00:21:30 -0500

    fix: imports never used

--- src/hooks/useDropdown.ts
+++ src/hooks/useDropdown.ts
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react';
 
 export const useDropdown = (initialTheme: string) => {
   const [isActive, setIsActive] = useState<boolean>(false);
