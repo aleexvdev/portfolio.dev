0c8045e34df55b89fdb431984a73ddd7f631011d
aleexvdev <valverdealex25@gmail.com>
2024-09-03 00:33:22 -0500

    feat: about section

---
+++ src/components/svelte/Times.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  const currentTime = new Date();
+  const hours = currentTime.getHours();
+  const minutes = currentTime.getMinutes();
+  const seconds = currentTime.getSeconds();
+
+  $: formattedTime = `${hours < 10 ? '0' + hours : hours}:${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}`;
+</script>
+
+<span class="text-[#22C55E] text-center text-nowrap font-bold text-4xl">{formattedTime}</span>
