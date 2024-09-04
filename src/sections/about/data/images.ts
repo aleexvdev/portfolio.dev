513fec3bab9dd6b2a5ff77e52a7ea38d33d2e7cd
aleexvdev <valverdealex25@gmail.com>
2024-09-04 00:16:43 -0500

    fix: route images skills

--- src/sections/about/data/images.ts
+++ src/sections/about/data/images.ts
@@ -1,105 +1,105 @@
 export const images = [
   {
     id: '1',
-    path: 'public/images/skills/javascript.png',
+    path: './images/skills/javascript.png',
     alt: 'Javascript',
     title: 'Javascript',
     description: 'Javascript'
   },
   {
     id: '2',
-    path: 'public/images/skills/typescript.png',
+    path: './images/skills/typescript.png',
     alt: 'Typescript',
     title: 'Typescript',
     description: 'Typescript'
   },
   {
     id: '3',
-    path: 'public/images/skills/php.png',
+    path: './images/skills/php.png',
     alt: 'PHP',
     title: 'PHP',
     description: 'PHP'
   },
   {
     id: '4',
-    path: 'public/images/skills/node-js.png',
+    path: './images/skills/node-js.png',
     alt: 'Node.js',
     title: 'Node.js',
     description: 'Node.js'
   },
   {
     id: '5',
-    path: 'public/images/skills/Express.js_dark.svg',
+    path: './images/skills/Express.js_dark.svg',
     alt: 'Express.js',
     title: 'Express.js',
     description: 'Express.js'
   },
   {
     id: '6',
-    path: 'public/images/skills/laravel.png',
+    path: './images/skills/laravel.png',
     alt: 'Laravel',
     title: 'Laravel',
     description: 'Laravel'
   },
   {
     id: '7',
-    path: 'public/images/skills/react.png',
+    path: './images/skills/react.png',
     alt: 'React',
     title: 'React',
     description: 'React'
   },
   {
     id: '8',
-    path: 'public/images/skills/angular.png',
+    path: './images/skills/angular.png',
     alt: 'Angular',
     title: 'Angular',
     description: 'Angular'
   },
   {
     id: '9',
-    path: 'public/images/skills/nextjs.png',
+    path: './images/skills/nextjs.png',
     alt: 'Next.js',
     title: 'Next.js',
     description: 'Next.js'
   }, 
   {
     id: '10',
-    path: 'public/images/skills/mysql.png',
+    path: './images/skills/mysql.png',
     alt: 'MySQL',
     title: 'MySQL',
     description: 'MySQL'
   },
   {
     id: '11',
-    path: 'public/images/skills/mongodb.png',
+    path: './images/skills/mongodb.png',
     alt: 'MongoDB',
     title: 'MongoDB',
     description: 'MongoDB'
   },
   {
     id: '12',
-    path: 'public/images/skills/postgresql.png',
+    path: './images/skills/postgresql.png',
     alt: 'PostgreSQL',
     title: 'PostgreSQL',
     description: 'PostgreSQL'
   }, 
   {
     id: '13',
-    path: 'public/images/skills/jest.png',
+    path: './images/skills/jest.png',
     alt: 'Jest',
     title: 'Jest',
     description: 'Jest'
   }, 
   {
     id: '14',
-    path: 'public/images/skills/docker.png',
+    path: './images/skills/docker.png',
     alt: 'Docker',
     title: 'Docker',
     description: 'Docker'
   }, 
   {
     id: '15',
-    path: 'public/images/skills/git.png',
+    path: './images/skills/git.png',
     alt: 'Git',
     title: 'Git',
     description: 'Git'
