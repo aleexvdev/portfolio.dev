a473cc7555cac7d2460b39f861458cc9268e9541
aleexvdev <valverdealex25@gmail.com>
2024-09-08 01:25:59 -0500

    fix: fix data socials

--- src/sections/contact/lib/data.ts
+++ src/sections/contact/lib/data.ts
@@ -4,35 +4,35 @@ export const socials: Social[] = [
   {
     id: 1,
     name: "Linkedin",
-    path: "https://www.linkedin.com/in/alexvalverde/",
+    path: "https://www.linkedin.com/in/alexvdev/",
     icon_dark: "./images/social/linkedin.svg",
     icon_light: "./images/social/linkedin.svg"
   },
   {
     id: 2,
     name: "Instagram",
-    path: "https://www.instagram.com/alexvalverde/",
+    path: "https://www.instagram.com/alexvalverde._/",
     icon_dark: "./images/social/instagram_dark.svg",
     icon_light: "./images/social/instagram_light.svg"
   },
   {
     id: 3,
     name: "Discord",
-    path: "https://discord.gg/8u9s7v9",
+    path: "https://discordapp.com/users/550184376441503744",
     icon_dark: "./images/social/discord.svg",
     icon_light: "./images/social/discord.svg"
   },
   /* {
     id: 4,
     name: "Threads",
-    path: "#",
+    path: "https://www.threads.net/@alexvalverde._",
     icon_dark: "./images/social/threads_dark.svg",
     icon_light: "./images/social/threads_light.svg"
   }, */
   {
     id: 5,
     name: "Skype",
-    path: "https://join.skype.com/invite/5e8c0c7d-d5f6-4f0a-b2e7-a2c2f5d4d0e1",
+    path: "https://join.skype.com/invite/ckbUFPk7oSqu",
     icon_dark: "./images/social/skype.svg",
     icon_light: "./images/social/skype.svg"
   },
