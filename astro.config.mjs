5e3a553299821a1b81065aca1fed43a7235c153f
aleexvdev <valverdealex25@gmail.com>
2024-10-05 18:46:17 -0500

    update versions

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ dotenv.config();
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), svelte()],
-  output: 'server',
+  output: 'hybrid',
   adapter: node({
     mode: 'standalone'
   }),
@@ -17,20 +17,5 @@ export default defineConfig({
     ssr: {
       noExternal: ['react-icons'],
     },
-    /* define: {
-      'import.meta.env.SMTP_HOST': JSON.stringify(process.env.SMTP_HOST),
-      'import.meta.env.SMTP_PORT': JSON.stringify(process.env.SMTP_PORT),
-      'import.meta.env.SMTP_SECURE': JSON.stringify(process.env.SMTP_SECURE),
-      'import.meta.env.SMTP_USER': JSON.stringify(process.env.SMTP_USER),
-      'import.meta.env.SMTP_PASS': JSON.stringify(process.env.SMTP_PASS),
-      'import.meta.env.FROM_NAME': JSON.stringify(process.env.FROM_NAME),
-      'import.meta.env.FROM_EMAIL': JSON.stringify(process.env.FROM_EMAIL),
-      'import.meta.env.TO_EMAIL': JSON.stringify(process.env.TO_EMAIL),
-    }, */
   },
-  /* server: {
-    proxy: {
-      '/api': 'http://localhost:4321',  // Proxy para el backend
-    },
-  }, */
 });
