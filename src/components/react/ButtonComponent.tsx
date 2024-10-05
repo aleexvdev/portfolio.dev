089272ccb2e61985c89c11f62017933ae510541f
aleexvdev <valverdealex25@gmail.com>
2024-10-04 21:18:57 -0500

    feat: contact form

--- src/components/react/ButtonComponent.tsx
+++ src/components/react/ButtonComponent.tsx
@@ -4,9 +4,11 @@ import { motion } from "framer-motion";
 interface ButtonComponentProps {
   children: React.ReactNode;
   type: "button" | "submit" | "reset";
+  disabled?: boolean;
+  onClick?: () => void;
 }
 
-export const ButtonComponent = ({ children }: ButtonComponentProps) => {
+export const ButtonComponent = ({ children, type, disabled, onClick }: ButtonComponentProps) => {
 
   const [isHovered, setIsHovered] = useState<boolean>(false);
 
@@ -15,11 +17,14 @@ export const ButtonComponent = ({ children }: ButtonComponentProps) => {
       className="relative w-full md:w-max flex items-center justify-center text-black dark:text-white/75 hover:dark:text-white bg-white/50 hover:bg-white dark:bg-black px-6 py-3 rounded-xl overflow-hidden transition-colors duration-300"
       onHoverStart={() => setIsHovered(true)}
       onHoverEnd={() => setIsHovered(false)}
+      disabled={disabled}
+      type={type}
+      onClick={onClick}
     >
       <motion.div
         className="absolute bottom-0 left-0 right-0 h-[2px]"
         initial={{ scaleX: 0 }}
-        animate={{ scaleX: isHovered ? 1 : 0 }}
+        animate={{ scaleX: isHovered && !disabled ? 1 : 0 }}
         transition={{ duration: 0.3, ease: "easeInOut" }}
         style={{
           background:
@@ -28,7 +33,7 @@ export const ButtonComponent = ({ children }: ButtonComponentProps) => {
       />
       <motion.div
         transition={{ duration: 0.3 }}
-        className="flex items-center justify-center space-x-3"
+        className={`flex items-center justify-center space-x-3 ${disabled ? "opacity-50" : ""}`}
       >
         {children}
       </motion.div>
--- src/sections/contact/components/TemplateForm.tsx
+++ src/sections/contact/components/TemplateForm.tsx
@@ -1,8 +1,50 @@
-import { AtSign, Mail, MessagesSquare, RefreshCw, Send, UserRoundCheck } from "lucide-react";
+import {
+  AtSign,
+  Mail,
+  MessagesSquare,
+  RefreshCw,
+  Send,
+  UserRoundCheck,
+} from "lucide-react";
 import { ButtonComponent } from "@/components/react/ButtonComponent";
 import { motion } from "framer-motion";
+import { useState } from "react";
 
 export const TemplateForm = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    name: "",
+    subject: "",
+    message: "",
+  });
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const isFormValid =
+    formData.email && formData.name && formData.subject && formData.message;
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("Formulario enviado:", formData);
+  };
+
+  // Función para resetear el formulario
+  const handleReset = () => {
+    setFormData({
+      email: "",
+      name: "",
+      subject: "",
+      message: "",
+    });
+  };
+
   return (
     <motion.div
       className="relative overflow-hidden w-full flex flex-col items-center justify-stretch rounded-3xl border border-gray-600/20 bg-[#F1F5F9] dark:bg-[#181818] transition-all duration-300 hover:shadow-xl"
@@ -21,126 +63,140 @@ export const TemplateForm = () => {
         </h2>
       </header>
       <div className="px-2 md:px-6 lg:px-8 pb-6 md:pb-6 lg:pb-8 w-full h-full">
-        <motion.div
-          className="h-full px-4"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 0.5, delay: 0.2 }}
-        >
-          <div className="w-full flex flex-col items-center justify-center space-y-6">
-            <motion.div
-              className="w-full border-b border-gray-600/20"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 0.5, delay: 0.4 }}
-            >
-              <div className="flex flex-col md:flex-row items-start md:items-center px-0 md:px-1">
-                <label
-                  htmlFor="email"
-                  className="text-black dark:text-white md:mr-2 mb-1 md:mb-0 min-w-[30px] text-base lg:text-lg font-medium flex items-center space-x-2"
-                >
-                  <AtSign className="w-5 h-5" />
-                  <span>Correo Electrónico:</span>
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  placeholder="sayayin.ss3@gmail.com"
-                  className="flex-grow bg-transparent text-stone-600 dark:text-stone-400 placeholder:text-stone-500 px-1 md:px-2 py-1 text-base lg:text-base focus:outline-none focus:border-none autofill-custom-light dark:autofill-custom-dark"
-                  required
-                />
-              </div>
-            </motion.div>
-
-            <motion.div
-              className="w-full border-b border-gray-600/20"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 0.5, delay: 0.6 }}
-            >
-              <div className="flex flex-col md:flex-row items-start md:items-center px-0 md:px-1">
-                <label
-                  htmlFor="name"
-                  className="text-black dark:text-white md:mr-2 mb-1 md:mb-0 min-w-[30px] text-base lg:text-lg font-medium flex items-center space-x-2"
-                >
-                  <UserRoundCheck className="w-5 h-5" />
-                  <span>Nombres:</span>
-                </label>
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  placeholder="Son Gokú"
-                  className="flex-grow bg-transparent text-stone-600 dark:text-stone-400 placeholder:text-stone-500 px-1 md:px-2 py-1 text-base lg:text-base focus:outline-none focus:border-none autofill-custom-light dark:autofill-custom-dark"
-                  required
-                />
-              </div>
-            </motion.div>
-
-            <motion.div
-              className="w-full border-b border-gray-600/20"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 0.5, delay: 0.8 }}
-            >
-              <div className="flex flex-col md:flex-row items-start md:items-center px-0 md:px-1">
-                <label
-                  htmlFor="subject"
-                  className="text-black dark:text-white md:mr-2 mb-1 md:mb-0 min-w-[30px] text-base lg:text-lg font-medium flex items-center space-x-2"
-                >
-                  <Mail className="w-5 h-5" />
-                  <span>Asunto:</span>
-                </label>
-                <input
-                  type="text"
-                  id="subject"
-                  name="subject"
-                  placeholder="Sobre tu último proyecto..."
-                  className="flex-grow bg-transparent text-stone-600 dark:text-stone-400 placeholder:text-stone-500 px-1 md:px-2 py-1 text-base lg:text-base focus:outline-none focus:border-none autofill-custom-light dark:autofill-custom-dark"
-                  required
-                />
-              </div>
-            </motion.div>
-
-            <motion.div
-              className="w-full flex flex-col items-center justify-start py-6 px-0 md:px-2"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 0.5, delay: 1 }}
-            >
-              <label
-                htmlFor="message"
-                className="text-black dark:text-white md:mr-2 mb-4 md:mb-4 min-w-[30px] text-base lg:text-lg font-medium w-full flex items-center space-x-2"
+        <form onSubmit={handleSubmit}>
+          <motion.div
+            className="h-full px-4"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5, delay: 0.2 }}
+          >
+            <div className="w-full flex flex-col items-center justify-center space-y-6">
+              <motion.div
+                className="w-full border-b border-gray-600/20"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.5, delay: 0.4 }}
               >
-                <MessagesSquare className="w-5 h-5" />
-                <span>Mensaje:</span>
-              </label>
-              <textarea
-                name="message"
-                id="message"
-                placeholder="Déjame saber tus pensamientos, preguntas o ideas..."
-                className="w-full min-h-80 resize-none outline-none border-none p-4 md:p-6 rounded-xl bg-white/50 dark:bg-black shadow-lg shadow-white dark:shadow-black text-base lg:text-base font-normal text-pretty text-stone-800 dark:text-stone-400 dark:placeholder-[#8d8d8d] placeholder-[#999999]"
-              ></textarea>
-            </motion.div>
+                <div className="flex flex-col md:flex-row items-start md:items-center px-0 md:px-1">
+                  <label
+                    htmlFor="email"
+                    className="text-black dark:text-white md:mr-2 mb-1 md:mb-0 min-w-[30px] text-base lg:text-lg font-medium flex items-center space-x-2"
+                  >
+                    <AtSign className="w-5 h-5" />
+                    <span>Correo Electrónico:</span>
+                  </label>
+                  <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    placeholder="sayayin.ss3@gmail.com"
+                    className="flex-grow bg-transparent text-stone-600 dark:text-stone-400 placeholder:text-stone-500 px-1 md:px-2 py-1 text-base lg:text-base focus:outline-none focus:border-none autofill-custom-light dark:autofill-custom-dark"
+                    required
+                  />
+                </div>
+              </motion.div>
 
-            <motion.div
-              className="w-full flex items-center justify-center md:justify-end py-2 space-x-6"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 0.5, delay: 1.2 }}
-            >
-              <ButtonComponent type="reset">
-                <RefreshCw className="w-5 h-5" />
-                <span className="text-base md:text-lg lg:text-xl font-medium">Reset</span>
-              </ButtonComponent>
-              <ButtonComponent type="submit">
-                <Send className="w-5 h-5" />
-                <span className="text-base md:text-lg lg:text-xl font-medium">Enviar</span>
-              </ButtonComponent>
-            </motion.div>
-          </div>
-        </motion.div>
+              <motion.div
+                className="w-full border-b border-gray-600/20"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.5, delay: 0.6 }}
+              >
+                <div className="flex flex-col md:flex-row items-start md:items-center px-0 md:px-1">
+                  <label
+                    htmlFor="name"
+                    className="text-black dark:text-white md:mr-2 mb-1 md:mb-0 min-w-[30px] text-base lg:text-lg font-medium flex items-center space-x-2"
+                  >
+                    <UserRoundCheck className="w-5 h-5" />
+                    <span>Nombres:</span>
+                  </label>
+                  <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    value={formData.name}
+                    onChange={handleChange}
+                    placeholder="Son Gokú"
+                    className="flex-grow bg-transparent text-stone-600 dark:text-stone-400 placeholder:text-stone-500 px-1 md:px-2 py-1 text-base lg:text-base focus:outline-none focus:border-none autofill-custom-light dark:autofill-custom-dark"
+                    required
+                  />
+                </div>
+              </motion.div>
+
+              <motion.div
+                className="w-full border-b border-gray-600/20"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.5, delay: 0.8 }}
+              >
+                <div className="flex flex-col md:flex-row items-start md:items-center px-0 md:px-1">
+                  <label
+                    htmlFor="subject"
+                    className="text-black dark:text-white md:mr-2 mb-1 md:mb-0 min-w-[30px] text-base lg:text-lg font-medium flex items-center space-x-2"
+                  >
+                    <Mail className="w-5 h-5" />
+                    <span>Asunto:</span>
+                  </label>
+                  <input
+                    type="text"
+                    id="subject"
+                    name="subject"
+                    value={formData.subject}
+                    onChange={handleChange}
+                    placeholder="Sobre tu último proyecto..."
+                    className="flex-grow bg-transparent text-stone-600 dark:text-stone-400 placeholder:text-stone-500 px-1 md:px-2 py-1 text-base lg:text-base focus:outline-none focus:border-none autofill-custom-light dark:autofill-custom-dark"
+                    required
+                  />
+                </div>
+              </motion.div>
+
+              <motion.div
+                className="w-full flex flex-col items-center justify-start py-6 px-0 md:px-2"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.5, delay: 1 }}
+              >
+                <label
+                  htmlFor="message"
+                  className="text-black dark:text-white md:mr-2 mb-4 md:mb-4 min-w-[30px] text-base lg:text-lg font-medium w-full flex items-center space-x-2"
+                >
+                  <MessagesSquare className="w-5 h-5" />
+                  <span>Mensaje:</span>
+                </label>
+                <textarea
+                  name="message"
+                  id="message"
+                  value={formData.message}
+                  onChange={handleChange}
+                  placeholder="Déjame saber tus pensamientos, preguntas o ideas..."
+                  className="w-full min-h-80 resize-none outline-none border-none p-4 md:p-6 rounded-xl bg-white/50 dark:bg-black shadow-lg shadow-white dark:shadow-black text-base lg:text-base font-normal text-pretty text-stone-800 dark:text-stone-400 dark:placeholder-[#8d8d8d] placeholder-[#999999]"
+                ></textarea>
+              </motion.div>
+
+              <motion.div
+                className="w-full flex items-center justify-center md:justify-end py-2 space-x-6"
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.5, delay: 1.2 }}
+              >
+                <ButtonComponent type="reset" onClick={handleReset}>
+                  <RefreshCw className="w-5 h-5" />
+                  <span className="text-base md:text-lg lg:text-xl font-medium">
+                    Reset
+                  </span>
+                </ButtonComponent>
+                <ButtonComponent type="submit" disabled={!isFormValid}>
+                  <Send className="w-5 h-5" />
+                  <span className="text-base md:text-lg lg:text-xl font-medium">
+                    Enviar
+                  </span>
+                </ButtonComponent>
+              </motion.div>
+            </div>
+          </motion.div>
+        </form>
       </div>
     </motion.div>
   );
